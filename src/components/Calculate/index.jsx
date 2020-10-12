fad32ba872b2085e077cb691d2b3cfabd99d8ebb
Jhonatan Lima <39390551+jhonyllima@users.noreply.github.com>
2020-10-12 13:13:17 -0300

    Create function handleClick

--- src/components/Calculate/index.jsx
+++ src/components/Calculate/index.jsx
@@ -7,55 +7,65 @@ import {
 } from './styles'
 
 function Calculate(){
-  const [number, setNumber] = useState([0]);
+  const [number, setNumber] = useState('');
 
   console.log('teste', number);
+
+  function handleClick(value){
+    setNumber(`${number}${value}`)
+
+    // if (setNumber(`${value}`) === 'x' || 'X'){
+    //   console.log('oia bixo',number * 2)
+    // }
+  }
+
+  console.log(number * 2);
 
   return (
     <Container>
       <ContentTop>
-        <input />
+        <input  value={number}/>
       </ContentTop>
 
       <Content>
         <div>
-          <button type="button" onClick={() => setNumber(7)}> 7 </button>
+          <button type="button" onClick={() => handleClick(7)}> 7 </button>
           
-          <button type="button" onClick={() => setNumber(8)}> 8 </button>
-          <button type="button" onClick={() => setNumber(9)}> 9  </button>
+          <button type="button" onClick={() => handleClick(8)}> 8 </button>
+          <button type="button" onClick={() => handleClick(9)}> 9  </button>
 
           <span>
-            <button  type="button" onClick={() => setNumber('/')}> /  </button>
+            <button  type="button" onClick={() => handleClick('/')}> /  </button>
           </span>
         </div>  
 
         <div>
-          <button type="button" onClick={() => setNumber(4)}> 4 </button>
-          <button type="button" onClick={() => setNumber(5)}> 5  </button>
-          <button type="button" onClick={() => setNumber(6)}> 6  </button>
+          <button type="button" onClick={() => handleClick(4)}> 4 </button>
+          <button type="button" onClick={() => handleClick(5)}> 5  </button>
+          <button type="button" onClick={() => handleClick(6)}> 6  </button>
 
           <span>
-            <button type="button" onClick={() => setNumber('x')}> x  </button>
+            <button type="button" onClick={() => handleClick('x')}> x  </button>
           </span>
         </div>
 
         <div>
-          <button type="button" onClick={() => setNumber(1)}> 1 </button>
-          <button type="button" onClick={() => setNumber(2)}> 2 </button>
-          <button type="button" onClick={() => setNumber(3)}> 3 </button>
+          <button type="button" onClick={() => handleClick(1)}> 1 </button>
+          <button type="button" onClick={() => handleClick(2)}> 2 </button>
+          <button type="button" onClick={() => handleClick(3)}> 3 </button>
 
           <span>
-            <button type="button" onClick={() => setNumber('+')}> + </button>
+            <button type="button" onClick={() => handleClick('+')}> + </button>
           </span>
         </div>
 
         <div> 
-          <button type="button" onClick={() => setNumber('.')}> . </button>
-          <button type="button" onClick={() => setNumber(0)}> 0 </button>
-          <button type="button" onClick={() => setNumber('=')}> = </button>
+          <button type="button" onClick={() => handleClick('.')}> . </button>
+          <button type="button" onClick={() => handleClick(0)}> 0 </button>
+          <button type="button" onClick={() => handleClick('=')}> = </button>
 
           <span>
-            <button type="button" onClick={() => setNumber('-')}> - </button>
+            <button type="button" onClick={() => handleClick('-')}> - </button>
           </span>
         </div>
       </Content>
